de1bb0570b2a6dbe615276e40141000c67710cd7
Suman Kumar Sah <sahsumankumar3@gmail.com>
2021-07-20 22:00:10 +0530

    Adding Spin Button and written code for the functionality!!

--- script.js
+++ script.js
@@ -1,32 +1,67 @@
+window.onload = function() {
 
-let slot1 = document.getElementById("slot1")
-let slot4 = document.getElementById("slot2")
-let slot3 = document.getElementById("slot3")
-
-let speedInput = document.getElementById("speedInput")
-
-let values = ['😎','😂','🥴','😢','🤑','🙄','😅','🤩','😭']
-
-function changeValue() {
-    return values[Math.floor(Math.random() * 9)]
-}
-
-let animationId;
-
-function animationReset(speedVal) {
-    if(animationId) clearInterval(animationId)
-
-    animationId = setInterval( function () {
-   
-        slot1.innerText = changeValue()
-        slot2.innerText = changeValue()
-        slot3.innerText = changeValue()
+    
+    let slot1 = document.getElementById("slot1")
+    let slot4 = document.getElementById("slot2")
+    let slot3 = document.getElementById("slot3")
+    let speedInput = document.getElementById("speedInput")
+    
+    let spinButton = document.getElementById('spinBtn')
+    
+    
+    
+    let values = ['😎','😂','🥴','😢','🤑','🙄','😅','🤩','😭']
+    
+    function changeValue() {
+        return values[Math.floor(Math.random() * 9)]
+    }
+    
+    let animationId;
+    
+    function animationReset(speedVal, timeDur) {
+        if(animationId) clearInterval(animationId)
+    
+        animationId = setInterval( function () {
+       
+            slot1.innerText = changeValue()
+            slot2.innerText = changeValue()
+            slot3.innerText = changeValue()
+            
+            },1000 * speedVal)
         
-        },1000 / speedVal)
-}
-
-speedInput.onchange = function (event) {
-    let currentSpeed = event.target.value;
-    document.documentElement.style.setProperty("--speed",currentSpeed)
-    animationReset(currentSpeed)
-}
+        let x=timeDur+1;
+        let timerId;
+        timerId = setInterval(() => {
+            --x;
+            spinButton.innerText = `${x}`;
+    
+            if(x === 0) {
+                clearInterval(animationId);
+                clearInterval(timerId);
+                document.documentElement.style.setProperty("--speed",0)
+                spinButton.innerText = "START SPINNING";
+    
+    
+                // return;
+            }
+        }, 1000);
+    }
+    
+    /* speedInput.onchange = function (event) {
+        let currentSpeed = event.target.value;
+        document.documentElement.style.setProperty("--speed",currentSpeed)
+        animationReset(currentSpeed)
+    } */
+    
+    spinButton.onclick = function(event) {
+    
+        let currentSpeed = 1.1 - (speedInput.value / 10);
+    
+        document.documentElement.style.setProperty("--speed",currentSpeed)
+    
+        let randomTime = Math.floor((Math.random() * 7)+ 8);
+        animationReset(currentSpeed , randomTime );
+    
+    }
+    }
+    
